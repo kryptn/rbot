6c7ad8376b48d0bb72ab31550643c581e83f00a8
David <kryptn@gmail.com>
2011-10-11 04:32:00 -0500

    handles multiple subreddits easier

--- bot.py
+++ bot.py
@@ -2,12 +2,26 @@
 import reddit
 cond = True
 r = reddit.Reddit(user_agent='eve_trial_stalker')
-stories = r.get_subreddit('eve').get_hot()
 
-while cond:
-	try:
-		story = stories.next()
-		if story.is_self and 'trial' in story.selftext:
-			print story.permalink
-	except StopIteration:
-		cond = False
+def findText(stories, text):
+	for story in stories:
+		if story.is_self:
+			if text in story.title or text in story.selftext:
+				print story.permalink
+
+def getStories(subs):
+	l = []
+	if type(subs) is str:
+		l = list(r.get_subreddit(subs).get_hot())
+	elif type(subs) is list:
+		for s in subs:
+			l += list(r.get_subreddit(s).get_hot())
+	else
+		return l
+
+subs = ['eve','evedreddit']
+
+stories = getStories(subs)
+
+findText(stories,'trial')
+findText(stories,'trial')
