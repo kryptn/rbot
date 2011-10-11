17607d4a2e5d899c4f704359e19896c34ad51fc5
David <kryptn@gmail.com>
2011-10-11 04:18:37 -0500

    added loop, changed condition

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 import reddit
-
+cond = True
 r = reddit.Reddit(user_agent='eve_trial_stalker')
-stories = r.get_subreddit('eve').get_new()
+stories = r.get_subreddit('eve').get_hot()
 
-try:
-	story = stories.next()
-	if story.is_self and 'trial' in story.selftext:
-		print 'heysomething'
-except StopIteration:
-	pass
+while cond:
+	try:
+		story = stories.next()
+		if story.is_self and 'trial' in story.selftext:
+			print story.permalink
+	except StopIteration:
+		cond = False
